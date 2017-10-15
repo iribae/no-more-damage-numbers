3d76f94a30e1f8badd406f4c7ec442fa3833c214
iribae <xiphion.iris@gmail.com>
2017-10-16 01:45:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const Command = require('command')
 module.exports = function blockSkill(dispatch) {
 	const command = Command(dispatch)
 	let enabled
-	command.add('rskill', () => { enabled = !enabled})
+	command.add('rskill', () => {
+		enabled = !enabled
+		command.message('damage spam garbo blocker '+(enabled?'enabled':'disabled')+'.')
+	})
 	dispatch.hook('S_EACH_SKILL_RESULT', () => {
 	  if (!enabled) return
 	return false
